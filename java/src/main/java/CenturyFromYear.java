0b947b17027e6557e139ec28284cb5fb247cd59b
Ilya Khadykin <ilya_khadykin@epam.com>
2019-01-02 17:56:36 +0300

    Day 2 - Add solution for centuryFromYear problem from Codesignal

---
+++ java/src/main/java/CenturyFromYear.java
@@ -0,0 +1,9 @@
+public class CenturyFromYear {
+
+    public static int centuryFromYear(int year) {
+        if (year % 100 == 0) {
+            return year / 100;
+        }
+        return year / 100 + 1;
+    }
+}
