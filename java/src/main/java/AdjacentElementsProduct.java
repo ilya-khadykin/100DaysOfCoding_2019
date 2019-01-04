0b0277d97e0201e4cf57d99d75015cb84dd0b553
Ilya Khadykin <ilya_khadykin@epam.com>
2019-01-04 18:37:35 +0300

    Day 4: Find max product of adjacent elements in an array (Java)

--- java/src/main/java/AdjacentElementsProduct.java
+++ java/src/main/java/AdjacentElementsProduct.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
-
 public class AdjacentElementsProduct {
+
+    // Read AdjacentElementsProduct.md for more details
+    public static int adjacentElementsProduct(int[] inputArray) {
+        int adjacentProduct = inputArray[0] * inputArray[1];
+        for(int i=0; i<inputArray.length-1; i++) {
+            if (adjacentProduct <  inputArray[i] * inputArray[i+1]) {
+                adjacentProduct = inputArray[i] * inputArray[i+1];
+            }
+        }
+        return adjacentProduct;
+    }
+
+    public static void main(String[] args) {
+        int[] inputArray1 = {3, 6, -2, -5, 7, 3};
+        assert 21 == adjacentElementsProduct(inputArray1);
+        int[] inputArray2 = {1, 0, 1, 0, 1000};
+        assert 0 == adjacentElementsProduct(inputArray2);
+        int[] inputArray3 = {1, 2, 3, 0};
+        assert 6 == adjacentElementsProduct(inputArray3);
+    }
+
 }
